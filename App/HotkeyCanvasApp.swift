fa0a851ebc12c7134de02dea22027aae2750b0db
Kazuhide Oki <kazuhideoki@users.noreply.github.com>
2026-02-14 09:44:38 +0900

    test input

--- App/HotkeyCanvasApp.swift
+++ App/HotkeyCanvasApp.swift
@@ -1,9 +1,23 @@
+import AppKit
 import InterfaceAdapters
 import SwiftUI
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_: Notification) {
+        #if DEBUG
+        NSApplication.shared.setActivationPolicy(.regular)
+        NSApplication.shared.activate(ignoringOtherApps: true)
+        DispatchQueue.main.async {
+            NSApplication.shared.windows.first?.makeKeyAndOrderFront(nil)
+        }
+        #endif
+    }
+}
 
 // TEMP: Entry scene for hello-world bootstrap. Replace root view with canvas feature UI.
 @main
 struct HotkeyCanvasApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     private let container = DependencyContainer()
 
     var body: some Scene {
