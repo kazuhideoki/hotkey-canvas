d24d8a1b8d29c9b272838794f68f6c386720dcb7
Kazuhide Oki <kazuhideoki@users.noreply.github.com>
2026-02-22 21:01:10 +0900

    resolve conflict

--- Sources/InterfaceAdapters/Output/ViewModel/CanvasViewModel.swift
+++ Sources/InterfaceAdapters/Output/ViewModel/CanvasViewModel.swift
@@ -173,13 +173,9 @@ extension CanvasViewModel {
         switch command {
         case .addNode, .addChildNode, .addSiblingNode:
             return true
-<<<<<<< HEAD
         case .alignParentNodesVertically,
             .moveFocus, .moveNode, .toggleFoldFocusedSubtree, .centerFocusedNode, .deleteFocusedNode,
-=======
-        case .moveFocus, .moveNode, .toggleFoldFocusedSubtree, .centerFocusedNode, .deleteFocusedNode,
             .copyFocusedSubtree, .cutFocusedSubtree, .pasteSubtreeAsChild,
->>>>>>> main
             .setNodeText, .toggleFocusedNodeMarkdownStyle, .convertFocusedAreaMode, .createArea,
             .assignNodesToArea:
             return false
